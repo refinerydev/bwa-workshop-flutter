05f0d9135263dcbfb050e60ce03d81c3041fb273
hpazk <hpazk.zkh@gmail.com>
2021-10-05 13:12:17 +0700

    update: homepage

--- lib/screen/home_screen.dart
+++ lib/screen/home_screen.dart
@@ -1,10 +1,124 @@
+import 'package:flutmov/theme.dart';
+import 'package:flutmov/widgets/movie_carousell.dart';
+import 'package:flutmov/widgets/movie_list_item.dart';
 import 'package:flutter/material.dart';
 
 class HomeScreen extends StatelessWidget {
+  Widget header() {
+    return Container(
+      margin: EdgeInsets.only(top: 33),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(
+                'Home',
+                style: blackTextStyle.copyWith(
+                  fontSize: 28.0,
+                  fontWeight: black,
+                ),
+              ),
+              SizedBox(
+                height: 4.0,
+              ),
+              Text(
+                'Watch much easier',
+                style: greyTextStyle.copyWith(
+                  fontSize: 16.0,
+                ),
+              ),
+            ],
+          ),
+          Container(
+            width: 55.0,
+            height: 45.0,
+            decoration: BoxDecoration(
+              color: whiteColor,
+              borderRadius: BorderRadius.horizontal(
+                left: Radius.circular(259.0),
+              ),
+            ),
+            child: Center(
+              child: Image.asset(
+                'assets/icon_search.png',
+                width: 22,
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      
+      backgroundColor: lightBackgroundColor,
+      body: SafeArea(
+        child: Container(
+          margin: EdgeInsets.only(
+            left: 24.0,
+          ),
+          child: SingleChildScrollView(
+            scrollDirection: Axis.vertical,
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                header(),
+                SingleChildScrollView(
+                  scrollDirection: Axis.horizontal,
+                  child: Row(
+                    children: [
+                      MovieCarousellItem(
+                        imageurl: 'assets/image_cover1.png',
+                        title: 'Jhn Wick 4',
+                        releaseDate: DateTime(2020, 5, 1),
+                        rating: 0,
+                      ),
+                      MovieCarousellItem(
+                        imageurl: 'assets/image_cover1.png',
+                        title: 'Jhn Wick 4',
+                        releaseDate: DateTime(2020, 5, 1),
+                        rating: 10,
+                      ),
+                    ],
+                  ),
+                ),
+                Container(
+                  margin: EdgeInsets.only(
+                    top: 30,
+                  ),
+                  child: Text(
+                    'From Disney',
+                    style: blackTextStyle.copyWith(
+                      fontSize: 24,
+                      fontWeight: black,
+                    ),
+                  ),
+                ),
+                Column(
+                  children: [
+                    MovieListItem(
+                      imageurl: 'assets/image_cover3.png',
+                      title: 'Mulan Session',
+                      releaseDate: DateTime(2021, 7, 17),
+                      rating: 10,
+                    ),
+                    MovieListItem(
+                      imageurl: 'assets/image_cover4.png',
+                      title: 'Beauty & The Beast',
+                      releaseDate: DateTime(2021, 7, 17),
+                      rating: 10,
+                    ),
+                  ],
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
     );
   }
 }
---
+++ lib/widgets/movie_list_item.dart
@@ -0,0 +1,107 @@
+import 'package:flutmov/theme.dart';
+import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
+
+class MovieListItem extends StatelessWidget {
+  final String imageurl;
+  final String title;
+  final DateTime releaseDate;
+  final double rating;
+
+  const MovieListItem({
+    Key? key,
+    required this.imageurl,
+    required this.title,
+    required this.releaseDate,
+    required this.rating,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: EdgeInsets.only(top: 20),
+      width: double.infinity,
+      height: 127,
+      child: Row(
+        children: [
+          Container(
+            width: 100,
+            height: 127,
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(21.0),
+              image: DecorationImage(
+                image: AssetImage(imageurl),
+                fit: BoxFit.cover,
+              ),
+            ),
+          ),
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(
+                title,
+                style: blackTextStyle.copyWith(
+                  fontSize: 20,
+                  fontWeight: bold,
+                ),
+              ),
+              SizedBox(
+                height: 4,
+              ),
+              Text(
+                DateFormat('MMM dd yyyy').format(releaseDate),
+                style: greyTextStyle.copyWith(
+                  fontSize: 16,
+                ),
+              ),
+              SizedBox(
+                height: 20,
+              ),
+              Row(
+                children: [
+                  Icon(
+                    Icons.star,
+                    color: rating >= 2 ? yellowColor : lightGreyColor,
+                    size: 18.0,
+                  ),
+                  SizedBox(
+                    width: 2.0,
+                  ),
+                  Icon(
+                    Icons.star,
+                    color: rating >= 4 ? yellowColor : lightGreyColor,
+                    size: 18.0,
+                  ),
+                  SizedBox(
+                    width: 2.0,
+                  ),
+                  Icon(
+                    Icons.star,
+                    color: rating >= 6 ? yellowColor : lightGreyColor,
+                    size: 18.0,
+                  ),
+                  SizedBox(
+                    width: 2.0,
+                  ),
+                  Icon(
+                    Icons.star,
+                    color: rating >= 8 ? yellowColor : lightGreyColor,
+                    size: 18.0,
+                  ),
+                  SizedBox(
+                    width: 2.0,
+                  ),
+                  Icon(
+                    Icons.star,
+                    color: rating >= 10 ? yellowColor : lightGreyColor,
+                    size: 18.0,
+                  ),
+                ],
+              )
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+}
